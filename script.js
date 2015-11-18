a234071bb2eef995720060102cea140c073ee53c
lamx <manon.lamoureux@gmail.com>
2015-11-18 18:07:41 +0000

    Second commit

--- script.js
+++ script.js
@@ -2,20 +2,26 @@ var bouton = document.getElementById('bouton'),
     affichage = document.getElementById('affichage'),
     score = 0,
     multiplicateur = document.getElementById('multiplicateur'),
-    compteur = 1;
+    compteur = 1,
+    autoclick = document.getElementById('autoclick'),
+    supercompteur = 0;
 
     bouton.addEventListener('click', function(){
-        score = score+compteur;
+        score = score + compteur;
         affichage.innerHTML = parseInt(score)+' cookies';
     });
 
+    function prix() {
+        return 20*compteur*compteur;
+    };
+
     multiplicateur.addEventListener('click', function incrementer() {
-        if (score > 49) {
-        compteur++;
-        score = score-50;
+        if (score >= prix()) {
+        score = score - prix();
+        compteur = compteur + 1;
         affichage.innerHTML = parseInt(score)+' cookies';
-        multiplicateur.innerHTML = 'Multiplicateur x'+parseInt(compteur);
+        multiplicateur.innerHTML = 'Multiplicateur x'+parseInt(compteur)+' (prix du prochain : '+prix()+' cookies)';
         } else {
-            alert('Vous n\'avez pas assez de cookies ! Un multiplicateur coûte 50 cookies.')
+            alert("Votre score est insuffisant !")
         }
     })
